101d2fd29628a66c46964fffa3c8f4e7064142c4
Edu <eduardo3q@gmail.com>
2017-09-13 00:58:29 +0200

    App\Models => App\Model

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature;
 
-use App\Models\User;
+use App\Model\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
